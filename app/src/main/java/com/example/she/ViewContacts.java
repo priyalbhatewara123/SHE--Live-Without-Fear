57158b035b651abee97a8e7fa88e6ff8f39ebb23
Mayank Ujawane <67855648+MayankUjawane@users.noreply.github.com>
2021-02-08 23:06:44 +0530

    Delete feature added with dialog box issue #18 (#30)
    * Delete feature added
    
    * added dialog box to confirm before deleting the contact
    
    * Update ViewContacts.java

--- app/src/main/java/com/example/she/ViewContacts.java
+++ app/src/main/java/com/example/she/ViewContacts.java
@@ -1,9 +1,12 @@
 package com.example.she;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Pair;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
@@ -20,37 +23,65 @@ public class ViewContacts extends AppCompatActivity implements ContactListAdapto
 
     ListView listView;
     DatabaseHandler db;
-    ArrayList<Pair<String,String>> contactList;
+    ArrayList<Pair<String, String>> contactList;
+    ContactListAdaptor contactListAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_contacts);
 
+        updateListView();
+    }
+
+    @Override
+    public void editOptionSelected(String phoneNumber) {
+        Toast.makeText(this, "Edit coming soon", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void deleteOptionSelected(final String phoneNumber) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         builder.setTitle("Delete")
+                .setMessage("Are you sure you want to delete?")
+                .setIcon(R.drawable.ic_delete_24)
+                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ViewContacts.this, "Contact Deleted", Toast.LENGTH_SHORT).show();
+                        db = new DatabaseHandler(ViewContacts.this);
+                        db.deleteContact(phoneNumber);
+                        updateListView();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+
+    }
+
+    //updates the list view
+    public void updateListView() {
         listView = findViewById(R.id.list_view);
         db = new DatabaseHandler(this);
         contactList = new ArrayList<>();
         Cursor cursor = db.getAllContacts();
-        if(cursor.getCount() == 0){
+
+        if (cursor.getCount() == 0) {
+            contactListAdaptor.clear();
+            contactListAdaptor.notifyDataSetChanged();
             Toast.makeText(this, "No contacts added!", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            while (cursor.moveToNext()){
-                Pair<String,String> contact = new Pair<>(cursor.getString(1),cursor.getString(2));
+        } else {
+            while (cursor.moveToNext()) {
+                Pair<String, String> contact = new Pair<>(cursor.getString(1), cursor.getString(2));
                 contactList.add(contact);
-                ContactListAdaptor contactListAdaptor = new ContactListAdaptor(this,contactList,this);
+                contactListAdaptor = new ContactListAdaptor(this, contactList, this);
                 listView.setAdapter(contactListAdaptor);
             }
         }
-    }
-
-    @Override
-    public void editOptionSelected(int position) {
-        Toast.makeText(this,"Edit coming soon",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void deleteOptionSelected(int position) {
-        Toast.makeText(this,"Delete coming soon",Toast.LENGTH_SHORT).show();
     }
 }
